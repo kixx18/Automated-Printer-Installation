b2e249e53251e322e5f890311e64a84ae0cf0dc6
kixx18 <123468014+kixx18@users.noreply.github.com>
2025-07-10 14:52:31 +0900

    Add files via upload

---
+++ IE11_Enable Script.ps1
@@ -0,0 +1,29 @@
+﻿Write-Host "
+#########################################################################
+             
+                  # Auto enable Internet Explorer 11 #
+                    # Windows Optional Feature #
+                    # Win 10 - Retail Machines #
+          # Troubleshoots: Unable to open links directly from Outlook App #
+                    # Developed by Aaron Kawahara #
+         
+#########################################################################"  
+       
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Warning "Please run this script as Administrator."
+    Read-Host -Prompt "Press Enter to exit"
+    exit
+}
+
+
+$ie11 = (Get-WindowsOptionalFeature -Online -FeatureName "Internet-Explorer-Optional").state
+
+    if ($ie11 -ne "Enabled") {
+        Write-Host "IE 11 not Enabled. Enabling Feature"
+        Enable-WindowsOptionalFeature -FeatureName "Internet-Explorer-Optional-amd64" -Online -NoRestart -Verbose
+}
+    else {
+        Write-Host "IE11 Feature already Enabled" -ForegroundColor Green
+        }
+
+Read-Host -Prompt "Press Enter to exit"
